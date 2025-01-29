2a9953e02799ef1310b609f89b50a3f83973f0f1
Jaman-M <jaman.manik1@gmail.comm>
2025-01-30 07:28:26 +0800

    create and setup the project; Load bottles Data and show in grid layout

---
+++ src/components/Bottle/Bottle.jsx
@@ -0,0 +1,17 @@
+import './Bottle.css'
+
+const Bottle = ({bottle}) => {
+    console.log(bottle);
+
+    const {name, img, price} = bottle;
+
+    return (
+        <div className="bottle">
+            <h3>Botttle: {name}</h3>
+            <img src={img} alt="" />
+            <p>Price: $ {price}</p>
+        </div>
+    );
+};
+
+export default Bottle;
